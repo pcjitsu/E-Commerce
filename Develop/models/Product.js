1122fe433d61a4a1c69a239de16b5042d661abf9
Antonios Zikos <antonioszikos@Antonioss-MBP.attlocal.net>
2023-09-05 16:27:41 -0500

    Models Completed

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -12,29 +12,36 @@ class Product extends Model {
 Product.init(
   {
     // define columns
-    id:{
+    id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
-    product_name:{
+    product_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    price:{
+    price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
-      validate:{
-        isDecimal: true,
-      },
-      stock:{
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 10,
-        validate:{
-          isNumeric: true,
-        },
+      validate: {
+        isDecimal: true, 
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      Validate: {
+        isNumeric: true, 
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
       }
     }
   },
--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -13,8 +13,21 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true
     },
-
-  },
+    product_id:{
+      type: DataTypes.INTEGER,
+      references:{
+        model:'product',
+        key:'id'
+      }
+    },
+    tag_id:{
+      type: DataTypes.INTEGER,
+      references:{
+        model:'tag',
+        key:'id'
+      }
+    },
+    },
   {
     sequelize,
     timestamps: false,
