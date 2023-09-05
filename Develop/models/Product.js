ff5075422adc4a9ce7d84991de6eefc6e6528c18
Antonios Zikos <antonioszikos@Antonioss-MBP.attlocal.net>
2023-09-04 21:07:01 -0500

    added Product Model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -4,12 +4,39 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model {
+  
+}
 
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price:{
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate:{
+        isDecimal: true,
+      },
+      stock:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+        validate:{
+          isNumeric: true,
+        },
+      }
+    }
   },
   {
     sequelize,
@@ -21,3 +48,46 @@ Product.init(
 );
 
 module.exports = Product;
+
+
+// * `Product`
+
+//   * `id`
+  
+//     * Integer.
+  
+//     * Doesn't allow null values.
+  
+//     * Set as primary key.
+  
+//     * Uses auto increment.
+
+//   * `product_name`
+  
+//     * String.
+  
+//     * Doesn't allow null values.
+
+//   * `price`
+  
+//     * Decimal.
+  
+//     * Doesn't allow null values.
+  
+//     * Validates that the value is a decimal.
+
+//   * `stock`
+  
+//     * Integer.
+  
+//     * Doesn't allow null values.
+  
+//     * Set a default value of `10`.
+  
+//     * Validates that the value is numeric.
+
+//   * `category_id`
+  
+//     * Integer.
+  
+//     * References the `Category` model's `id`.
