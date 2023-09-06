5e545ed5a0b1aa8262ccdefe9043ba1b701ecd5a
Antonios Zikos <antonioszikos@Antonioss-MBP.attlocal.net>
2023-09-05 21:09:52 -0500

    Working on getting single product data

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -25,10 +25,26 @@ try{
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+try{
+  singleProductData = await Product(req.params.id, {include: [ {
+    model: Category,
+    attributes: ['category_name']
+},
+{
+    model: Tag,
+    attributes: ['tag_name']
+
+}
+]});
+res.status(200).json(singleProductData);
+}catch (err){
+res.status(500).json(err);
+}
 });
+
 
 // create new product
 router.post('/', (req, res) => {
