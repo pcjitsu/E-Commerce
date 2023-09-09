c0784ae3f3996fbecf414d0d6790b7ac68fa220b
Antonios Zikos <antonioszikos@Antonioss-MBP.attlocal.net>
2023-09-09 18:21:02 -0500

    finishing up routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,18 +3,51 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-});
+  try{
+  const tagData = await Tag.findAll({include: [
+    {
+        model: Product,
+        attributes: ['product_name']
+    },
+    {
+        model: ProductTag,
+        attributes: ['product_id', 'tag_id']
+    }  
+  ]});
+  res.json(tagDataData);
+}
+  catch (err) {
+    res.status(500).json(err);
+  }
+  });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-});
+  try{
+  const singleTagData = await Tag.findByPk(req.params.id, {include: [ {
+    model: Product,
+    attributes: ['product_name'] 
+},
+{
+    model: ProductTag,
+    attributes: ['product_id', 'tag_id']
+
+}
+]});
+res.json(categoryData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+  });
 
 router.post('/', (req, res) => {
   // create a new tag
+
 });
 
 router.put('/:id', (req, res) => {
