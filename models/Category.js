daaf4eff01dc084e9db791f4a4e74c4956f88bf2
Antonios Zikos <antonioszikos@Antonioss-MBP.attlocal.net>
2023-09-24 21:00:17 -0500

    removed Develop folder

--- models/Category.js
+++ models/Category.js
@@ -1,28 +1,31 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../config/connection.js');
+const sequelize = require('../../config/connection.js');
 
-class Tag extends Model {}
+class Category extends Model {}
 
-Tag.init(
+Category.init(
   {
+    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
-    tag_name: {
+    category_name: {
       type: DataTypes.STRING,
-    }
+      allowNull: false,
+    },
+
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: 'category',
   }
 );
 
-module.exports = Tag;
+module.exports = Category;
